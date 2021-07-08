28465c1728d92c1f7258368cd6c1e7722730fc07
Thilina-k <thilina.20200822@iit.ac.lk>
2021-07-08 09:16:17 +0530

    Update player.java

--- player.java
+++ player.java
@@ -10,4 +10,18 @@ public class player {
     public player(String name,int age,String type,int runsScored,int wicketsTaken){
 
     }
+	
+	public double getBattingAvg(){
+		double battingAvg = this.runsScored/15;
+		return battingAvg;
+	}
+	
+	public void getPlayerDetails(){
+		System.out.println("Player name            :"+this.name);
+		System.out.println("Player age             :"+this.age);
+		System.out.println("Player type            :"+this.type);
+		System.out.println("Player runs scored     :"+this.runsScored);
+		System.out.println("Player wickets taken   :"+this.wicketsTaken);
+	}
+	
 }
